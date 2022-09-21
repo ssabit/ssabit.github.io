c07934dfed6e3e1dd82a39c938cb6fa64b80f491
Saad Ibna Omar Sabit <45698632+ssabit@users.noreply.github.com>
2022-09-21 20:11:05 +0600

    update and bug fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,56 @@
-//Strict Mode 
-(function($) {
-  "use strict";
+//Strict Mode
+(function ($) {
+  'use strict';
 
-//Run on Document Ready
-$(document).ready(function(){  
+  //Run on Document Ready
+  $(document).ready(function () {
+    //Smooth scrool
+    $('html').niceScroll({ styler: 'fb', cursorcolor: '#000' });
 
-  //Smooth scrool
-  $("html").niceScroll({styler:"fb",cursorcolor:"#000"});
+    //Side menu - Open
+    $('.side-menu-open').mouseenter(function () {
+      $('.side-menu').animate({ left: '0px' }, 600, 'easeOutCubic');
+    });
 
-  //Side menu - Open
-  $('.side-menu-open').mouseenter(function(){
-    $('.side-menu').animate({'left': '0px'}, 600, 'easeOutCubic');
-  });
+    //Side menu - Close
+    $('#side-menu-close').click(function () {
+      var sideWidth = $('.side-menu').outerWidth();
+      var sideWidthClose = '-' + sideWidth + 'px';
+      $('.side-menu').animate({ left: sideWidthClose }, 600, 'easeOutCubic');
+      preventDefault();
+    });
 
-  //Side menu - Close
-  $('#side-menu-close').click(function(){
-    var sideWidth = $('.side-menu').outerWidth();
-    var sideWidthClose = '-' + sideWidth + 'px';
-    $('.side-menu').animate({'left': sideWidthClose}, 600, 'easeOutCubic');
-    preventDefault();
-  });
-
-  //Smooth Scroll on anchor links
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 700, 'easeInOutExpo');
-        return false;
+    //Smooth Scroll on anchor links
+    $('a[href*=#]:not([href=#])').click(function () {
+      if (
+        location.pathname.replace(/^\//, '') ==
+          this.pathname.replace(/^\//, '') &&
+        location.hostname == this.hostname
+      ) {
+        var target = $(this.hash);
+        target = target.length
+          ? target
+          : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html,body').animate(
+            {
+              scrollTop: target.offset().top,
+            },
+            700,
+            'easeInOutExpo'
+          );
+          return false;
+        }
       }
-    }
-  });
+    });
 
-  //Bootstrap Scroll Spy
-  $('[data-spy="scroll"]').each(function () {
-    var $spy = $(this).scrollspy('refresh');
-  });  
+    //Bootstrap Scroll Spy
+    $('[data-spy="scroll"]').each(function () {
+      var $spy = $(this).scrollspy('refresh');
+    });
 
-  //Bxslider -see options at http://bxslider.com/
-  $('.portfolio-itens').bxSlider({
+    //Bxslider -see options at http://bxslider.com/
+    $('.portfolio-itens').bxSlider({
       slideWidth: 200,
       minSlides: 1,
       maxSlides: 4,
@@ -56,100 +65,106 @@ $(document).ready(function(){
       easing: 'easeOutElastic',
       pager: false,
       prevText: '<i class="fa fa-chevron-left"></i>',
-      nextText: '<i class="fa fa-chevron-right"></i>'
-  });
+      nextText: '<i class="fa fa-chevron-right"></i>',
+    });
 
-  
-  //Nivo Lightbox
-  $('a.nivobox').nivoLightbox({ effect: 'fade' });
+    //Nivo Lightbox
+    $('a.nivobox').nivoLightbox({ effect: 'fade' });
 
-  //Portfolio Animations
-  $('.portfolio-item').hover(function(){
-    $(this).find('.hover-bg-wrapper').fadeIn(200);
-    $(this).find('.hover').show();  
-    $(this).find('p').addClass('animated').addClass('fadeInUp');
-  }, function(){
-    $(this).find('.hover-bg-wrapper').fadeOut(200);
-    $(this).find('.hover').fadeOut(200);
-    $(this).find('p').removeClass('fadeInUp');
-  });
+    //Portfolio Animations
+    $('.portfolio-item').hover(
+      function () {
+        $(this).find('.hover-bg-wrapper').fadeIn(200);
+        $(this).find('.hover').show();
+        $(this).find('p').addClass('animated').addClass('fadeInUp');
+      },
+      function () {
+        $(this).find('.hover-bg-wrapper').fadeOut(200);
+        $(this).find('.hover').fadeOut(200);
+        $(this).find('p').removeClass('fadeInUp');
+      }
+    );
 
-  //Contact Form Validator and Ajax Sender
-  $("#contactForm").validate({
-    submitHandler: function(form) {
-      $.ajax({
-        type: "POST",
-        url: "php/contact-form.php",
-        data: {
-          "name": $("#contactForm #name").val(),
-          "email": $("#contactForm #email").val(),
-          "subject": $("#contactForm #subject").val(),
-          "message": $("#contactForm #message").val()
-        },
-        dataType: "json",
-        success: function (data) {
-          if (data.response == "success") {
-            $("#contactSuccess").fadeIn(300);
-            $("#contactError").addClass("hidden");
+    //Contact Form Validator and Ajax Sender
+    $('#contactForm').validate({
+      submitHandler: function (form) {
+        $.ajax({
+          type: 'POST',
+          url: 'php/contact-form.php',
+          data: {
+            name: $('#contactForm #name').val(),
+            email: $('#contactForm #email').val(),
+            subject: $('#contactForm #subject').val(),
+            message: $('#contactForm #message').val(),
+          },
+          dataType: 'json',
+          success: function (data) {
+            if (data.response == 'success') {
+              $('#contactSuccess').fadeIn(300);
+              $('#contactError').addClass('hidden');
 
-            $("#contactForm #name, #contactForm #email, #contactForm #subject, #contactForm #message")
-              .val("")
-              .blur()
-              .closest(".control-group")
-              .removeClass("success")
-              .removeClass("error");              
-            
-          } else {
-            $("#contactError").fadeIn(300);
-            $("#contactSuccess").addClass("hidden");
-          }
-        }
+              $(
+                '#contactForm #name, #contactForm #email, #contactForm #subject, #contactForm #message'
+              )
+                .val('')
+                .blur()
+                .closest('.control-group')
+                .removeClass('success')
+                .removeClass('error');
+            } else {
+              $('#contactError').fadeIn(300);
+              $('#contactSuccess').addClass('hidden');
+            }
+          },
+        });
+      },
+    });
 
-      });
-    }
-  });
+    //Modal for Contact Form
+    $('.modal-wrap').click(function () {
+      $('.modal-wrap').fadeOut(300);
+    });
 
-  //Modal for Contact Form
-  $('.modal-wrap').click(function(){
-    $('.modal-wrap').fadeOut(300);
-  });      
-
-  //Background Height fix for vertical progress
-  $( ".full-height" ).each(function() {
-    var $stretch = $(this);
-    $stretch.css({ height: $stretch.closest('.line').find('.content-wrap').height() });
-  }); 
-
-});
-
-//Run on Window Load
-$(window).load(function(){
-  //Page loader
-  $('#page-loader').fadeOut(200, function(){});
-
-  //Safari Crossbrowser animation Fix
-  if ($('html').hasClass('safari')) {
-      $('#content-body').removeClass('animated');
-  }
-
-  //Fade In load
-  $('#content-body').addClass('fadeInUp');
-
-  //Background Height fix for vertical progress
-  setTimeout(function () {    
-      $( ".full-height" ).each(function() {
-        var $stretch = $(this);
-        $stretch.css({ height: $stretch.closest('.line').find('.content-wrap').outerHeight() });
-      });  
-    }, 300
-  );
-  
-  //Background Height fix for vertical progress on window resize
-  $(window).resize(function(){ 
-     $( ".full-height" ).each(function() {
+    //Background Height fix for vertical progress
+    $('.full-height').each(function () {
       var $stretch = $(this);
-      $stretch.css({ height: $stretch.closest('.line').find('.content-wrap').outerHeight() });
-    }); 
+      $stretch.css({
+        height: $stretch.closest('.line').find('.content-wrap').height(),
+      });
+    });
   });
-});
+
+  //Run on Window Load
+  $(window).load(function () {
+    //Page loader
+    $('#page-loader').fadeOut(200, function () {});
+
+    //Safari Crossbrowser animation Fix
+    if ($('html').hasClass('safari')) {
+      $('#content-body').removeClass('animated');
+    }
+
+    //Fade In load
+    $('#content-body').addClass('fadeInUp');
+
+    //Background Height fix for vertical progress
+    setTimeout(function () {
+      $('.full-height').each(function () {
+        var $stretch = $(this);
+        $stretch.css({
+          height: $stretch.closest('.line').find('.content-wrap').outerHeight(),
+        });
+      });
+    }, 300);
+
+    //Background Height fix for vertical progress on window resize
+    $(window).resize(function () {
+      $('.full-height').each(function () {
+        var $stretch = $(this);
+        $stretch.css({
+          height: $stretch.closest('.line').find('.content-wrap').outerHeight(),
+        });
+      });
+    });
+  });
 })(jQuery);
--- assets/js/user.js
+++ assets/js/user.js
@@ -1,58 +1,38 @@
-(function($) {
+(function ($) {
+  function emailIsValid(email) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  }
 
-var message = "";
-var name = "";
-var email = "";
-var subject = "";
+  $('#sendMessage').on('click', function () {
+    var name = $('#name').val();
+    var email = $('#email').val();
+    var subject = $('#subject').val();
+    var message = $('#message').val();
 
-//var isvalid=0;
-
-
-function emailIsValid (email) {
-  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
-}
-
-
-$("#sendMessage").on("click", function() {
-
-
-  var name = $( '#name' ).val();
-  var email = $( '#email' ).val();
-  var subject = $( '#subject' ).val();
-  var message = $( '#message' ).val();
-
-
-
-if(name=="" || email=="" ||subject=="" ||message==""){
-  $("#contactError").show();
-  return false;
-
-}else if(emailIsValid(email)==false ){
-  $("#emailError").show();
-return false;
-}else{
-  $.ajax({
-
-    url: "https://formspree.io/xpzyjglk",
-    method: "POST",
-    data: {
-      name:name,
-      _replyto:email,
-      _subject:subject,
-      message: message
-
-    },
-    dataType: "json"
-
-},
-);
-}
-    $("#contactSuccess").show();
+    if (name == '' || email == '' || subject == '' || message == '') {
+      $('#contactError').show();
       return false;
-});
+    } else if (emailIsValid(email) == false) {
+      $('#emailError').show();
+      return false;
+    } else {
+      $.ajax({
+        url: 'https://formspree.io/xpzyjglk',
+        method: 'POST',
+        data: {
+          name: name,
+          _replyto: email,
+          _subject: subject,
+          message: message,
+        },
+        dataType: 'json',
+      });
+    }
+    $('#contactSuccess').show();
+    return false;
+  });
 
-$("#contactform").each(function(){
-  this.reset();
-});
-
+  $('#contactform').each(function () {
+    this.reset();
+  });
 })(jQuery);
